39f5fbc699c4ff00161dec18aacd3664cab937ab
John William Davis <jwdavisdeveloper@gmail.com>
2021-06-22 09:34:08 -0700

    Setup the first few pages, personalize index page

---
+++ src/pages/papa.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+// markup
+const PapaPage = () => {
+  return (
+    <h1>
+      The Dad
+      <span role='img' aria-label='Emoji of a Running Shirt'>
+        🎽
+      </span>
+    </h1>
+  );
+};
+
+export default PapaPage;
