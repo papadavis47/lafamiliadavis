3a8b3343f0ec245015618688470b959dd658f6ed
John William Davis <jwdavisdeveloper@gmail.com>
2021-07-04 17:20:40 -0700

    Hide Home Link with activeClassName, add abuelito page

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,37 +22,37 @@ const Header = () => {
       <nav className={styles.navbar}>
         <ul className={styles.navList}>
           <li className={styles.navListItem}>
-            <Link className={styles.link} to='/john' activeClassName='active'>
+            <Link className={styles.link} to='/john' activeClassName='active-link'>
               John
             </Link>{" "}
           </li>
           <li className={styles.navListItem}>
-            <Link className={styles.link} to='/inma' activeClassName='active'>
+            <Link className={styles.link} to='/inma' activeClassName='active-link'>
               Inma
             </Link>{" "}
           </li>
           <li className={styles.navListItem}>
-            <Link className={styles.link} to='/diego' activeClassName='active'>
+            <Link className={styles.link} to='/diego' activeClassName='active-link'>
               Diego
             </Link>{" "}
           </li>
           <li className={styles.navListItem}>
-            <Link className={styles.link} to='/santiago' activeClassName='active'>
+            <Link className={styles.link} to='/santiago' activeClassName='active-link'>
               Santiago
             </Link>{" "}
           </li>
           <li className={styles.navListItem}>
-            <Link className={styles.link} to='/linda' activeClassName='active'>
+            <Link className={styles.link} to='/linda' activeClassName='active-link'>
               Linda
             </Link>{" "}
           </li>
           <li className={styles.navListItem}>
-            <Link className={styles.link} to='/'>
+            <Link className={styles.link} to='/abuelitos' activeClassName='active-link'>
               <p className={styles.specialLink}>Abuelitos</p>
             </Link>
           </li>
           <li className={styles.navListItem}>
-            <Link className={styles.link} to='/' activeClassName='active'>
+            <Link className={styles.link} to='/' activeClassName='home-link'>
               Home
             </Link>
           </li>
---
+++ src/pages/abuelitos.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Intro from "../components/Intro";
+import Layout from "../components/Layout";
+
+// markup
+const AbuelitosPage = () => {
+  return (
+    <Layout pageTitle="Abuelitos' Page">
+      <Intro name='Victor Fuentes y Inmaculada Cruz' emoji='🎨' label='Artist pallete emoji' />
+    </Layout>
+  );
+};
+
+export default AbuelitosPage;
